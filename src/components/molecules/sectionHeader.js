7aae7f4938f82d089e2c4101cf658578ee85c0e9
WFROTAN <wfrotan@myseneca.ca>
2021-02-27 21:54:32 -0500

    fixed button below header section issue

--- src/components/molecules/sectionHeader.js
+++ src/components/molecules/sectionHeader.js
@@ -60,7 +60,7 @@ const HeaderContainer = styled.header`
   display: flex;
   flex-direction: column;
   position: relative;
-  height: 120vh;
+  height: 135vh;
   padding: 0 var(--spacingContent);
 
   @media ${media.lg} {
